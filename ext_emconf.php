ec2c8480833d85f4aee56bf29f48848eeed0f73a
Markus Mächler <markus.maechler@bithost.ch>
2017-01-20 12:10:39 +0100

    Updates description

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
 	'title' => 'PDF ViewHelpers',
-	'description' => 'Provides ViewHelpers to create PDF documents.',
+	'description' => 'Provides various Fluid ViewHelpers to create PDF documents. It is possible to use existing PDFs as template and extend them using these ViewHelpers. Under the hood pdfviewhelpers uses TCPDF and FPDI.',
 	'category' => 'fe',
 	'author' => 'Markus Mächler, Esteban Marin',
 	'author_email' => 'markus.maechler@bithost.ch, esteban.marin@bithost.ch',
