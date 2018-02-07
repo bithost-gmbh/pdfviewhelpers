ddc2204ac7af68cd4cda565273ddb2e262025fa9
Markus Mächler <markus.maechler@bithost.ch>
2018-02-07 15:11:52 +0100

    Fixes indentation

--- Classes/Hooks/TypoScriptFrontendController.php
+++ Classes/Hooks/TypoScriptFrontendController.php
@@ -5,18 +5,18 @@ namespace Bithost\Pdfviewhelpers\Hooks;
 use TYPO3\CMS\Core\SingletonInterface;
 
 class TypoScriptFrontendController implements SingletonInterface {
-
-    /**
-     * Prevent any output when a pdf is rendered, especially any headers being set!
-     *
-     * @param	array		$params Parameters from frontend
-     * @param	object		$ref TSFE object
-     * @return	void
-     */
-    function isOutputting(&$params, $ref)	{
-        if (isset($params['pObj']->applicationData['tx_pdfviewhelpers']['pdfOutput'])
-            && $params['pObj']->applicationData['tx_pdfviewhelpers']['pdfOutput'] === true)	{
-            $params['enableOutput'] = false;
-        }
-    }
+	/**
+	 * Prevent any output when a pdf is rendered, especially any headers being set!
+	 *
+	 * @param	array		$params Parameters from frontend
+	 * @param	object		$ref TSFE object
+	 * @return	void
+	 */
+	function isOutputting(&$params, $ref) {
+		if (isset($params['pObj']->applicationData['tx_pdfviewhelpers']['pdfOutput'])
+			&& $params['pObj']->applicationData['tx_pdfviewhelpers']['pdfOutput'] === true
+		) {
+			$params['enableOutput'] = false;
+		}
+	}
 }
