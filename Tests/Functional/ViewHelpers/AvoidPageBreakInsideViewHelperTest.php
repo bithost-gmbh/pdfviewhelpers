104bcf970424f4db2f0786eeb66daccaa6fe07d8
Markus Klösges <mkloesges@gmx.de>
2019-12-04 10:23:49 +0100

    [TASK] Add failing test for using custom fonts in AvoidPageBreakInsideViewHelper

--- Tests/Functional/ViewHelpers/AvoidPageBreakInsideViewHelperTest.php
+++ Tests/Functional/ViewHelpers/AvoidPageBreakInsideViewHelperTest.php
@@ -125,4 +125,20 @@ class AvoidPageBreakInsideViewHelperTest extends AbstractFunctionalTest
         $this->assertContains('text1', $pages[0]->getText());
         $this->assertContains('text2', $pages[1]->getText());
     }
+
+    /**
+     * @test
+     */
+    public function testCustomFont()
+    {
+        $this->setUpPage([$this->getFixturePath('AvoidPageBreakInsideViewHelper/CustomFont.txt')]);
+
+        $output = $this->renderFluidTemplate(
+            $this->getFixturePath('AvoidPageBreakInsideViewHelper/CustomFont.html'),
+            [
+                'text1' => 'text1',
+                'text2' => 'text2',
+            ]
+        );
+    }
 }
