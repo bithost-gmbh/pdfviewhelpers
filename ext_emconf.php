667eaa9c4e2456f183419c959e39ab2fbe9a1c83
Markus Mächler <markus.maechler@bithost.ch>
2020-04-21 14:35:25 +0200

    Updates version constraints for TYPO3 10 LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '2.3.0',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '8.7.0-10.3.99',
+			'typo3' => '8.7.0-10.4.99',
 			'php' => '7.0.0-0.0.0',
 		],
 		'conflicts' => [],
