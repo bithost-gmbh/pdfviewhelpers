840c608676063a81af3fda692d3a4fab142e0793
Markus Mächler <markus.maechler@bithost.ch>
2021-10-31 14:24:23 +0100

    Limits StopOutputMiddleware to TYPO3 versions >= 11

--- Classes/Middleware/Frontend/StopOutputMiddleware.php
+++ Classes/Middleware/Frontend/StopOutputMiddleware.php
@@ -9,6 +9,8 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\NullResponse;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class StopOutputMiddleware implements MiddlewareInterface
 {
@@ -16,7 +18,9 @@ class StopOutputMiddleware implements MiddlewareInterface
     {
         $response = $handler->handle($request);
 
-        if ($GLOBALS['TSFE']->applicationData['tx_pdfviewhelpers']['pdfOutput'] ?? false) {
+        if (GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() >= 11
+            && $GLOBALS['TSFE']->applicationData['tx_pdfviewhelpers']['pdfOutput'] ?? false
+        ) {
             return new NullResponse();
         }
 
