a982f2edec2e7fe09365cfb6948c4c4d76e14363
Markus Mächler <markus.maechler@bithost.ch>
2025-04-28 22:07:05 +0200

    Migrate StopOutputMiddleware from TSFE to OutputService

--- Classes/Middleware/Frontend/StopOutputMiddleware.php
+++ Classes/Middleware/Frontend/StopOutputMiddleware.php
@@ -4,24 +4,52 @@ declare(strict_types=1);
 
 namespace Bithost\Pdfviewhelpers\Middleware\Frontend;
 
+/* * *
+ *
+ * This file is part of the "PDF ViewHelpers" Extension for TYPO3 CMS.
+ *
+ *  (c) 2016 Markus Mächler <markus.maechler@bithost.ch>, Bithost GmbH
+ *           Esteban Gehring <esteban.gehring@bithost.ch>, Bithost GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * * */
+
+use Bithost\Pdfviewhelpers\Service\OutputService;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\NullResponse;
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class StopOutputMiddleware implements MiddlewareInterface
 {
+    public function __construct(
+        protected OutputService $outputService,
+    ) {
+    }
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = $handler->handle($request);
 
-        if (
-            GeneralUtility::makeInstance(Typo3Version::class)->getMajorVersion() >= 11
-            && ($GLOBALS['TSFE']->applicationData['tx_pdfviewhelpers']['pdfOutput'] ?? false)
-        ) {
+        if ($this->outputService->isOutputDestinationOut()) {
+            // Avoid sending additional request header or body data if TCPDF has already sent a response
             return new NullResponse();
         }
 
