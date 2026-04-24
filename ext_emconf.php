998d51e77818e075fc40846bc85c639af782ca4b
Markus Mächler <markus.maechler@bithost.ch>
2026-04-24 18:46:07 +0200

    Raises TYPO3 v14 version LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '3.1.1',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '12.4.0-13.4.99',
+			'typo3' => '13.4.0-14.3.99',
 			'php' => '8.2.0-0.0.0',
 		],
 		'conflicts' => [],
